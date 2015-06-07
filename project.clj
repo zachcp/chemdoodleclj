e409503557d7affcb674b452f79b8ec51cd08fbe
zachcp <zcharlop@rockefeller.du>
2015-06-06 21:07:13 -0400

    project.clj updates

--- project.clj
+++ project.clj
@@ -2,10 +2,22 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3123"]]
+                 [org.clojure/clojurescript "0.0-3123"]
+                 [reagent "0.5.0"]
+                 [cljsjs/kemia "0.2.0"]
+                 [cljsjs/chemdoodle "7.0.1"]
+                 ]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :node-dependencies [[source-map-support "0.2.8"]]
-  :plugins [[lein-npm "0.4.0"]]
-  :source-paths ["src" "target/classes"]
+  :plugins [[lein-npm "0.4.0"]
+            [lein-cljsbuild "1.0.6"]]
+  :source-paths ["src"]
   :clean-targets ["out" "release"]
-  :target-path "target")
+  :target-path "target"
+  :cljsbuild {
+     :builds [{
+         :source-paths ["src"]
+         :compiler {
+           :output-to "out/chemdoodleclj.js"
+           :optimizations :none
+           :pretty-print true}}]})
