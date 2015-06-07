19391a76158400b923401ef1b1a23a97ceaf50aa
zachcp <zcharlop@rockefeller.du>
2015-06-06 21:45:57 -0400

    match react and cljs dependencies to reagents

--- project.clj
+++ project.clj
@@ -2,22 +2,21 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3123"]
+                 [org.clojure/clojurescript "0.0-2816"]
                  [reagent "0.5.0"]
+                 [cljsjs/react "0.12.2-5"]
                  [cljsjs/kemia "0.2.0"]
                  [cljsjs/chemdoodle "7.0.1"]
                  ]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
-  :node-dependencies [[source-map-support "0.2.8"]]
-  :plugins [[lein-npm "0.4.0"]
-            [lein-cljsbuild "1.0.6"]]
+  :plugins [[lein-cljsbuild "1.0.6"]]
   :source-paths ["src"]
-  :clean-targets ["out" "release"]
+  :clean-targets ["out"]
   :target-path "target"
   :cljsbuild {
      :builds [{
          :source-paths ["src"]
          :compiler {
-           :output-to "out/chemdoodleclj.js"
-           :optimizations :none
+           :output-to "out/main.js"
+           :optimizations :whitespace
            :pretty-print true}}]})
